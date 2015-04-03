dd9ec5759bb68bcb8e35be6125baae1dcc3f8eba
Michael R. Shannon <mrshannon.aerospace@gmail.com>
2015-04-02 22:21:03 -0600

    Add non-verbosity to prime.

--- prime.c
+++ prime.c
@@ -1,49 +1,90 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <stdbool.h>
+#include <unistd.h>
 #include "primes.h"
 
 
 
-int main(int argc, char* argv[]){
 
-    // check argument count
-    if (argc < 2 || argc > 3){
-        fprintf(stderr, "ERROR: Expected 1 or two inputs.\n");
-        fprintf(stderr,
-                "Usage: %s <number of primes> <optional output file>\n",
-                argv[0]);
+int main(int argc, char* argv[])
+{
+    bool verbose = false;
+
+    int c;
+    opterr = 0;
+    while ((c = getopt(argc, argv, "hv")) != -1){
+        switch (c){
+            case 'h':
+                fprintf(stdout, 
+"NAME\n"
+"       prime - computes prime numbers\n"
+"\n"
+"SYNOPSIS\n"
+"       %s [-h] [-v] primes [output]\n"
+"\n"
+"DESCRIPTION\n"
+"       Takes the number of primes to generate and uses the rather\n"
+"       non-optimized algorithm found at\n"
+"           http://docs.cython.org/src/tutorial/cython_tutorial.html\n"
+"\n"
+"OPTIONS\n"
+"       -h      Display this message.\n"
+"\n"
+"       -v      Enable verbose output.  Prints each prime to the terminal\n"
+"               instead of just the last.  Does not apply if an output file\n"
+"               is given.\n"
+"\n"
+"       If the output argument is given the entire list of prime numbers\n"
+"       generated will be written as 4 byte unsigned integers to the\n"
+"       specified file.\n"
+"\n"
+"updated: Thursday, April 02, 2015\n",
+                        argv[0]);
+                return EXIT_SUCCESS;
+                break;
+            case 'v':
+                verbose = true;
+                break;
+        }
+    }
+
+    // make sure at least one argument is given.
+    if (optind >= argc){
+        fprintf(stderr, "Expected number of primes.\n");
         return EXIT_FAILURE;
     }
 
     // parse first argument
     size_t num_primes = 0;
-    if (sscanf(argv[1], "%zu", &num_primes) != 1){
-        fprintf(stderr, "Expected integer argument.\n");
+    if (sscanf(argv[optind++], "%zu", &num_primes) != 1){
+        fprintf(stderr, "Expected integer argument for primes.\n");
     }
 
-    // calculate primes
-    if (argc == 3){
+    // check for output file
+    if (optind < argc){
 
         // save results to file
-        if (primes_file(num_primes, argv[2])){
+        if (primes_file(num_primes, argv[optind++])){
             return EXIT_FAILURE;
+        } else {
+            return EXIT_SUCCESS;
         }
-
-    } else {
-
-        // print results to screen
-        printf("Calculating %zu primes.\n\n", num_primes);
-        unsigned int *prime_nums = malloc(num_primes*sizeof(unsigned int));
-        if (prime_nums == NULL){
-            fprintf(stderr, "ERROR: Failed to allocate memory.\n");
-            return EXIT_FAILURE;
-        }
-        primes_verbose(num_primes, prime_nums);
-        free(prime_nums);
-
     }
 
-
+    // print results to screen
+    unsigned int *prime_nums = malloc(num_primes*sizeof(unsigned int));
+    if (prime_nums == NULL){
+        fprintf(stderr, "ERROR: Failed to allocate memory.\n");
+        return EXIT_FAILURE;
+    }
+    if (verbose){
+        primes_verbose(num_primes, prime_nums);
+    } else {
+        primes(num_primes, prime_nums);
+        printf("%d\n", prime_nums[num_primes - 1]);
+    }
+    free(prime_nums);
 
     return EXIT_SUCCESS;
 
